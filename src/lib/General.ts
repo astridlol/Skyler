4279d9187033c7a7e2df04b4546913661b24c753
astridlol <sites@astrid.sh>
2023-11-01 13:36:30 -0400

    chore: removed unused things

--- src/lib/General.ts
+++ src/lib/General.ts
@@ -1,5 +1,3 @@
-import { APIEmbed } from 'discord.js';
-
 const prettify = (s: string, titleCase: boolean = false) => {
 	let newString = s.replace(/(_|-)/gi, ' ');
 	newString = newString.charAt(0).toUpperCase() + newString.slice(1);
@@ -11,35 +9,4 @@ const prettify = (s: string, titleCase: boolean = false) => {
 			.join(' ');
 };
 
-function replacePlaceholders(
-	s: string,
-	map: Map<string, string>,
-	characters: string = '%%'
-): string {
-	let placeholded = s;
-
-	for (const [key, value] of map.entries()) {
-		const regex = new RegExp(`${characters[0]}${key}${characters[1]}`, 'g');
-		placeholded = placeholded.replace(regex, value);
-	}
-
-	return placeholded;
-}
-
-function parseEmbed(obj: APIEmbed, map: Map<string, string>): APIEmbed {
-	const result: APIEmbed = {};
-
-	for (const key in obj) {
-		if (Object.prototype.hasOwnProperty.call(obj, key)) {
-			if (key === 'color') {
-				result[key] = obj[key];
-				continue;
-			}
-			result[key] = replacePlaceholders(obj[key], map);
-		}
-	}
-
-	return result;
-}
-
-export { prettify, replacePlaceholders, parseEmbed };
+export { prettify };
